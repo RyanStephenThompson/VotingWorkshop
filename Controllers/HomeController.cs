0e1f3640fd45eef124462e84abf63015e925f532
Ryan Thompson <ryan.thompson1225@gmail.com>
2024-02-13 17:21:46 +0200

    Created a static candidates view. This will display the description of a canididate and their image.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace VotingWorkshop.Controllers
             return View();
         }
 
+        public IActionResult Candidates()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
