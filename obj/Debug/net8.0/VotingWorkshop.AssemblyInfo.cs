b0b49be21019148ed07cb1d4000536c38e104b4f
Ryan Thompson <ryan.thompson1225@gmail.com>
2024-02-18 12:04:40 +0200

    update

--- obj/Debug/net8.0/VotingWorkshop.AssemblyInfo.cs
+++ obj/Debug/net8.0/VotingWorkshop.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VotingWorkshop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d7c5ec51a1372cf7472348da470137937b6e587")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a29645ee36997daad2c57b6430c56eb372c50419")]
 [assembly: System.Reflection.AssemblyProductAttribute("VotingWorkshop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VotingWorkshop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
