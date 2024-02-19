b2cfa61c6f1b38ae6f9ff3c6ee7dae8cd4131f5c
Ryan Thompson <ryan.thompson1225@gmail.com>
2024-02-19 20:09:08 +0200

    merge auth into master

--- obj/Debug/net8.0/VotingWorkshop.AssemblyInfo.cs
+++ obj/Debug/net8.0/VotingWorkshop.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VotingWorkshop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c338f8c5549e4339541a5465e6bd61902ef30097")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+884b9de51cf85867e7ebd7d5e31427c915c7d0d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("VotingWorkshop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VotingWorkshop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
